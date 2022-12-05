569dd9632c72299ab3647f13f851ee082bb4cd48
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2022-12-05 21:07:18 +0700

    first commit

---
+++ src/main/java/wsb/b/wsb/b/Product.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wsb.b.wsb.b;
+
+/**
+ *
+ * @author ALYA
+ */
+public class Product {
+    private String Id;
+    private String Name;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    
+    
+}
---
+++ src/test/java/wsb/b/wsb/b/ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.b.wsb.b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
